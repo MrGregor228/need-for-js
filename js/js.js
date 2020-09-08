ecd229ef08c1565382e0bb9e52c6c5e7a1c6da64
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2020-09-08 14:05:33 +0300

    1 commit

---
+++ js/js.js
@@ -0,0 +1,42 @@
+const score = document.querySelector('.score'),
+    start = document.querySelector('.start'),
+    gameArea = document.querySelector('.gameArea'),
+    car = document.createElement('div');
+
+car.classList.add('car');
+
+const keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowRight: false,
+    ArrowLeft: false
+};
+const settings = {
+    start:false,
+    score:0,
+    speed:3
+};
+const startGame = () => {
+    start.classList.add('hide');
+    settings.start = true;
+    gameArea.appendChild(car);
+    requestAnimationFrame(playGame);
+};
+const playGame = () => {
+    console.log('Play game!');
+    if (settings.start) {
+        requestAnimationFrame(playGame);
+    }    
+};
+const startRun = event => {
+    event.preventDefault();
+    keys[event.key] = true;
+};
+const stopRun = event => {
+    event.preventDefault();
+    keys[event.key] = false;
+};
+
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
